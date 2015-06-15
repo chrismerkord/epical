f25a04ecb6b5c1b3c906d610b3c9b3db92a5bfc0
Chris Merkord <chris@merkord.com>
2015-06-15 10:19:25 -0500

    Merged epi_week and epi_year functions

--- R/epiweek.R
+++ R/epiweek.R
@@ -1,21 +1,34 @@
 
-#' Calculate the Epidemiological Week of Calendar Dates
+#' Calculate Epidemiological Weeks from Calendar Dates
 #'
-#' Function to calculate the epidemiological week in which calendar
+#' Function to calculate the epidemiological week and year in which calendar
 #' dates fall.
 #'
 #' Epidemiological weeks, also called "epi-weeks" or "epi weeks", are a
-#' standardized method of assigning days to a week of the year. Here we define
-#' an epi-week as starting on a Sunday and ending on a Saturday, with the first
-#' epi-week of the year ending on the first Saturday of the year that falls at
-#' least four days into the year. In other words, the first epi-week always
-#' begins on the Sunday that falls in the range of December 29 to January 4.
+#' standardized method of assigning days to a week of the year. Epi-weeks are
+#' used by the WHO, CDC, and many other health organizations.
+#'
+#' Here we define an epi-week as starting on a Sunday and ending on a Saturday,
+#' with the first epi-week of the year ending on the first Saturday of the year
+#' that falls at least four days into the year. In other words, the first
+#' epi-week always begins on the Sunday that falls in the range of December 29
+#' to January 4.
+#'
+#' Because some dates in epi-week 1 can fall within the previous
+#' calendar year, and dates in epi-week 52/53 can fall within the next calendar
+#' year, it is necessary to specify both the epi-week number and an
+#' "epi-year" so that dates can be grouped correctly.
 #'
 #' The function first converts \code{date} to a \code{Date} object using the
 #' function \code{as.Date()}. I welcome suggestions for making this more robust.
 #'
 #' @param date A vector that can be coerced to a \code{Date}
-#' @return A numeric vector
+#' @return A data frame with columns labeled "epi-week" and "epi-year". The
+#' object also has class tbl_df and tbl for those who use the \code{dplyr}
+#' package.
+#'
+#' Each item in the parameter vector corresponds to a row in the returned data
+#' frame.
 #'
 #' @examples
 #' epi_week("2015-01-01")
@@ -27,78 +40,31 @@
 epi_week <- function(date) {
 
   date <- as.Date(date)
-  sapply(date, function(x) {
-    # wday returns the day of the week as a decimal number (01-07, Sunday is 1)
-    saturday_on_or_after_x <- x + (7 - lubridate::wday(x))
-    sunday_on_or_before_x <- x - (lubridate::wday(x) - 1)
-    if (lubridate::yday(saturday_on_or_after_x) %in% 4:10) {
-      # this is by definitation, because  the first epidemiological week of the
-      # year ends on the first Saturday of January, provided that it falls at
-      # least four days into the month. thus the Saturday ending the first
-      # epi-week must fall on January 4th through 10th
-      y <- 1
-    } else {
-      # in the case of the last epi-week of the year, modding the Sat. after
-      # would yield an epi-week of 1 instead of 52/53, so instead we have to
-      # mod the Sun. before
-      y <- ((lubridate::yday(sunday_on_or_before_x) + 2) %/% 7) + 1
-    }
-    return(y)
-  })
+  dplyr::bind_rows(
+    lapply(date, function(x) {
+      # wday returns the day of the week as a decimal number (1-7, Sunday is 1)
+      saturday_on_or_after_x <- x + (7 - lubridate::wday(x))
+      sunday_on_or_before_x <- x - (lubridate::wday(x) - 1)
+      if (lubridate::yday(saturday_on_or_after_x) %in% 4:10) {
+        # this is by definitation, because  the first epidemiological week of
+        # the year ends on the first Saturday of January, provided that it falls
+        # at least four days into the month. thus the Saturday ending the first
+        # epi-week must fall on January 4th through 10th
+        epi_week <- 1
+        epi_year <- lubridate::year(saturday_on_or_after_x)
+      } else {
+        # in the case of the last epi-week of the year, modding the Sat. after
+        # would yield an epi-week of 1 instead of 52/53, so instead we have to
+        # mod the Sun. before
+        epi_week <- ((lubridate::yday(sunday_on_or_before_x) + 2) %/% 7) + 1
+        epi_year <- lubridate::year(sunday_on_or_before_x)
+      }
+      return(data.frame(epi_week, epi_year))
+    })
+  )
 }
 
-#' Calculate the Epidemiological Year of Calendar Dates
-#'
-#' Function to calculate the epidemiological year in which calendar
-#' dates fall.
-#'
-#' Epidemiological weeks, also called "epi-weeks" or "epi weeks", are a
-#' standardized method of assigning days to a week of the year. The first
-#' epi-week always begins on the Sunday that falls in the range of December
-#' 29 to January 4. Thus dates in the first epidemiological week of the
-#' year sometimes fall within the previous calendar year. This is a problem if
-#' you want to group daily data into epi-weeks and years.
-#'
-#' Enter the "epi-year", which starts on the first day of epidemiological week 1
-#' and ends on the last day before the next epidemiological year starts.
-#'
-#' The function first converts \code{x} to a \code{Date} object using the
-#' function \code{as.Date()}. I welcome suggestions for making this more robust.
-#'
-#' @param date An object that can be converted to a \code{Date} by \code{as.Date()}
-#' @return A numeric vector
-#'
-#' @examples
-#' epi_year(c("2015-01-03", "2015-01-04"))
-#' epi_year(c("2013-12-28", "2013-12-29"))
-#'
-#' @family epi calendar functions
-#'
-
-
-epi_year <- function(date) {
-  date <- as.Date(date)
-  sapply(date, function(x) {
-    # wday returns the day of the week as a decimal number (01-07, Sunday is 1)
-    saturday_on_or_after_x <- x + (7 - lubridate::wday(x))
-    sunday_on_or_before_x <- x - (lubridate::wday(x) - 1)
-    if (lubridate::yday(saturday_on_or_after_x) %in% 4:10) {
-      # this is by definitation, because  the first epidemiological week of the
-      # year ends on the first Saturday of January, provided that it falls at
-      # least four days into the month. thus the Saturday ending the first
-      # epi-week must fall on January 4th through 10th
-      y <- lubridate::year(saturday_on_or_after_x)
-    } else {
-      # in the case of the last epi-week of the year, modding the Sat. after
-      # would yield an epi-week of 1 instead of 52/53, so instead we have to
-      # mod the Sun. before
-      y <- lubridate::year(sunday_on_or_before_x)
-    }
-    return(y)
-  })
-}
-
-#' Calculate the Start Date of an Epidemiological Year
+#' Calculate the Start Dates of Epidemiological Years
 #'
 #' Function to calculate the start dates of epidemiological years.
 #'
@@ -130,7 +96,7 @@ epi_year_start_date <- function(year) {
   return(start_date)
 }
 
-#' Calculate the Start Date of an Epidemiological Week
+#' Calculate the Start Dates of Epidemiological Weeks
 #'
 #' Function to calculate the start dates of epidemiological weeks.
 #'
